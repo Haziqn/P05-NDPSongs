8a7ded5f69699c887191b5763440b514cab1a577
darrellboey <15056233@myrp.edu.sg>
2017-05-23 15:36:45 +0800

    setting up activity main

--- app/src/main/java/com/example/a15017523/p05_ndpsongs/MainActivity.java
+++ app/src/main/java/com/example/a15017523/p05_ndpsongs/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.a15017523.p05_ndpsongs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnShow;
+    EditText etTitle, etSinger ,etYear;
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInsert = (Button)findViewById(R.id.btnInsert);
+        btnShow = (Button)findViewById(R.id.btnShow);
+        etTitle = (EditText)findViewById(R.id.etTitle);
+        etSinger = (EditText)findViewById(R.id.etSinger);
+        etYear = (EditText)findViewById(R.id.etYear);
+
+        
+
+
+
     }
 }
