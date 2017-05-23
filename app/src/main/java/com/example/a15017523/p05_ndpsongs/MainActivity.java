795f65d8826f5dffdc6ba6588405681c861c0b4f
darrellboey <15056233@myrp.edu.sg>
2017-05-23 16:10:40 +0800

    finish coding of main activity

--- app/src/main/java/com/example/a15017523/p05_ndpsongs/MainActivity.java
+++ app/src/main/java/com/example/a15017523/p05_ndpsongs/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.a15017523.p05_ndpsongs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnInsert, btnShow;
     EditText etTitle, etSinger ,etYear;
     RadioGroup rg;
+    RadioButton rb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,37 @@ public class MainActivity extends AppCompatActivity {
         etSinger = (EditText)findViewById(R.id.etSinger);
         etYear = (EditText)findViewById(R.id.etYear);
 
-        
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                rg = (RadioGroup) findViewById(R.id.rg);
+                // Get the Id of the selected radio button in the RadioGroup
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                // Get the radio button object from the Id we had gotten above
+                rb = (RadioButton) findViewById(selectedButtonId);
+
+                String title = etTitle.getText().toString();
+                String singers = etSinger.getText().toString();
+                int year = Integer.parseInt(etYear.getText().toString());
+                int selected = rg.getCheckedRadioButtonId();
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long row_affected = dbh.insertSong(title,singers,year,selected);
+                dbh.close();
+
+                if (row_affected != -1){
+                    Toast.makeText(MainActivity.this, "Insert successful",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            }
+        });
 
 
 
